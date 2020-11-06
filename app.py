69af7107c100a6f266e90232a44c296971ef8793
Karanja Mutahi <mutahi.henry@gmail.com>
2020-11-06 17:32:10 +0300

    Adde sqlite

--- app.py
+++ app.py
@@ -7,6 +7,13 @@
 
 
 from pyfingerprint.pyfingerprint import Pyfingerprint
+import sqlite3 as db
+
+#Global Vars
+db_active = false
+
+def print_lcd(message):
+  print(message)
 
 def init_fingerprint_sensor():
   try:
@@ -15,12 +22,51 @@ def init_fingerprint_sensor():
        print_lcd('Wrong Sensor Password')
        raise Exception('Wrong Sensor Password')
     return f 
-  except Exception as e:
-    lcd_print('Sensor Init Failed')
+ except Exception as e:
+    print_lcd('Sensor Init Failed')
     exit()
+
+def init_db():
+  print_lcd('Init DB')
+  try:
+    db_conn = db.connect('student_fingerprint.db')
+    cursor = db_conn.cursor()
+    cursor.close()
+    return db_conn
+  except db.Error as error:
+   print_lcd('DB Init failed')
+   return False
+
+def create_table(db_instance):
+  try:
+    query = '''CREATE TABLE students (id INTEGER PRIMARY KEY, 
+                                      fName TEXT NOT NULL, 
+                                      surname TEXT NOT NULL, 
+                                      registration TEXT NOT NULL, 
+                                      fingerprint_position INTEGER NOT NULL, 
+                                      active INTEGER);
+                                      '''
+    cursor = db_instance.cursor()
+    print("SQLite Connection Established")
+    cursor.execute(query)
+    db_instance.commit()
+    print("Table Created")
+    cursor.close()
+  except Exception as e:
+    print("Error while connecting to DB", e)
+  finally:
+    db_instance.close()
+  
+def read_fingerprint(f):
+  try:
+    f.readImage()
+  except Exception as e:
+    print_lcd('Read Print: FAIL')
 
 def setup():
   f = init_fingerprint_sensor()
+  db_inst = init_db()
+  create_table(db_inst)
   
 def loop():
   pass
