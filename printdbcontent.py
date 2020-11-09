7dff392b6600cde81f926ded74912642b20162ce
evansonoka05.j@gmail.com <evansonoka05.j@gmail.com>
2020-11-09 13:15:32 +0300

    printing db content

---
+++ printdbcontent.py
@@ -0,0 +1,11 @@
+import sqlite3 as lite
+con = lite.connect('student_fingerprint.db')
+
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM students")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
